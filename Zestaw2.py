d6854e8e488af5aa2cfa3df11cd548676f6193b8
Kamil Kantorski <kamil.k.kantorski@gmail.com>
2020-09-28 16:34:32 +0200

    Zestaw2 - Final version

--- Zestaw2.py
+++ Zestaw2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 27 22:37:55 2020
+Created on Mon Sep 28 15:13:36 2020
 
 @author: rotnak
 """
@@ -12,195 +12,63 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from sklearn import linear_model
-from scipy import stats
-from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('survey_results_public.csv')
-schema_df = pd.read_csv('survey_results_schema.csv')
+# Selecting columns from survey
+df_reg = pd.read_csv('survey_results_public.csv',
+                     usecols=['Age', 'YearsCode', 'YearsCodePro'])
 
-#df.info()
-
-pd.set_option('display.max_columns', 85)
-
-#Selecting columns from survey
-df_reg = pd.read_csv('survey_results_public.csv', usecols = ['Age', 'YearsCode', 'YearsCodePro'])
-df_reg2 = pd.read_csv('survey_results_public.csv', usecols = ['Age', 'YearsCode', 'YearsCodePro', 'Employment', 'Hobbyist'])
-#Removing rows without values
-df_reg.dropna(inplace = True)
-df_reg2.dropna(inplace = True)
+# Removing rows without values
+df_reg.dropna(inplace=True)
 
 
-#Verifying all values are numerical
+# Verifying all values are numerical
 column_values = df_reg[['YearsCodePro']].values.ravel()
 unique_values = pd.unique(column_values)
-print(unique_values)
 
 column_values = df_reg[['YearsCode']].values
 unique_values = np.unique(column_values)
-print(unique_values)
 
-
-#replacing string to int values
+# replacing string to int values
 df_reg.replace(to_replace={'Less than 1 year': '0',
-                              'More than 50 years': '51'},
-                  inplace=True)
+                           'More than 50 years': '51'},
+               inplace=True)
 
-df_reg2.replace(to_replace={'Less than 1 year': '0',
-                              'More than 50 years': '51'},
-                  inplace=True)
+# setting appropriate data types
+df_reg = df_reg.astype({'YearsCodePro': 'int64', 'YearsCode': 'int64'},
+                       copy=False)
 
-#replacing Yes/No to numerical values
-df_reg2.replace(to_replace={'No': '0',
-                              'Yes': '1'},
-                  inplace=True)
-
-#setting appropriate data types
-df_reg = df_reg.astype({'YearsCodePro': 'int64', 'YearsCode': 'int64'} , copy=False)
-#df_reg = df_reg.astype('float64', copy=False)
-print(df_reg['YearsCode'])
-print(df_reg['YearsCodePro'])
-print(df_reg2['Hobbyist'])
-
-#one hot encoding for Employment column
-Empl = LabelBinarizer().fit_transform(df_reg2.Employment)
-print(Empl)
-
-# df_reg2 = pd.concat([df_reg2, pd.get_dummies(df_reg2['Employment'])])
-# print(df_reg2)
-
-#Verifying correlation
+# Correlation with outliers
+print()
+print("Correlation with outliers")
 print(df_reg.corr())
 
-
-#plots
-# plt.plot(df_reg['YearsCodePro'], df_reg['Age'],'ro', markersize=0.3)
-# plt.ylabel('YearsCodePro')
-# plt.xlabel('Age')
-# plt.plot(df_reg['YearsCodePro'], df_reg['Hobbyist'],'ro', markersize=0.3, color = 'green')
-# plt.ylabel('YearsCodePro')
-# plt.xlabel('Hobbyist')
-# plt.plot(df_reg['YearsCodePro'], Empl,'ro', markersize=0.3, color = 'yellow')
-# plt.ylabel('YearsCodePro')
-# plt.xlabel(Empl)
-# plt.plot(df_reg['YearsCodePro'], df_reg['YearsCode'],'ro', markersize=0.3, color = 'blue')
-# plt.ylabel('YearsCodePro')
-# plt.xlabel('YearsCode/Age')
-# plt.show()
-
-print(df_reg.dtypes)
-
-#Looking for outliers
-
-sns.boxplot(y='YearsCodePro', data=df_reg)
-plt.show();
-sns.boxplot(y='YearsCode', data=df_reg)
-plt.show();
-sns.boxplot(y='Age', data=df_reg)
-plt.show();
-
-fig, ax = plt.subplots(figsize=(16,8))
-ax.scatter(df_reg['YearsCode'], df_reg['YearsCodePro'])
-ax.set_xlabel('YearsCode')
-ax.set_ylabel('YearsCodePro')
+# Boxplot with outliers
+sns.boxplot(y='YearsCodePro', data=df_reg).set(
+                              ylabel='YearsCodePro - outliers included')
 plt.show()
 
-fig, ax = plt.subplots(figsize=(16,8))
-ax.scatter(df_reg['Age'], df_reg['YearsCodePro'])
-ax.set_xlabel('Age')
-ax.set_ylabel('YearsCodePro')
-plt.show()
-
-#z-score with absolute value
-z = np.abs(stats.zscore(df_reg))
-print(z)
-
-#looking at outliers (>3)
-threshold = 3
-print(np.where(z > 3))
-
-#keeping values within 3 standard deviations
-df_reg_sd = df_reg[np.abs(df_reg - df_reg.mean()) <= 3*df_reg.std()]
-
-sns.boxplot(y='YearsCodePro', data=df_reg_sd)
-plt.show();
-df_reg_sd.plot()
-plt.show();
-
-print(df_reg_sd.corr())
-
-#interquartile range (IQR)/middle 50%
+# interquartile range (IQR)/middle 50%
 
 Q1 = df_reg.quantile(0.25)
 Q3 = df_reg.quantile(0.75)
 IQR = Q3 - Q1
 
-df_reg_q = df_reg[~((df_reg < (Q1 - 1.5 * IQR)) | (df_reg > (Q3 + 1.5 * IQR))).any(axis=1)]
+df_reg_q = df_reg[~((df_reg < (Q1 - 1.5 * IQR)) |
+                    (df_reg > (Q3 + 1.5 * IQR))).any(axis=1)]
 
-sns.boxplot(y='YearsCodePro', data=df_reg_q)
-plt.show();
-df_reg_q.plot()
-plt.show();
+# boxplot without outliers
+sns.boxplot(y='YearsCodePro', data=df_reg_q).set(
+                              ylabel='YearsCodePro - no outliers')
+plt.show()
 
 print()
 print("Correlation without outliers")
 print(df_reg_q.corr())
 
-#linear regression
-
-#one variable
-reg = linear_model.LinearRegression()
-reg.fit(df_reg[['YearsCode']], df_reg['YearsCodePro']);
-
-print()
-print("predicted values for one variable")
-print(reg.predict(df_reg[['YearsCode']]))
-
-y_pred1 = reg.predict(df_reg[['YearsCode']])
-y_true = df_reg['YearsCodePro']
-
-print()
-print("MSE for one variable:")
-print(mean_squared_error(y_true, y_pred1))
-
-
-
-#two variables
-reg2 = linear_model.LinearRegression()
-reg2.fit(df_reg[['YearsCode', 'Age']], df_reg['YearsCodePro']);
-
-print("coef:")
-print(reg2.coef_)
-
-print()
-print("Predicted values for two variables:")
-print(reg2.predict(df_reg[['YearsCode', 'Age']]))
-
-y_pred = reg2.predict(df_reg[['YearsCode', 'Age']])
-y_true = df_reg['YearsCodePro']
-
-print()
-print("MSE for two variables:")
-print(mean_squared_error(y_true, y_pred))
-
-#one variable without outliers
-reg_q = linear_model.LinearRegression()
-reg_q.fit(df_reg_q[['YearsCode']], df_reg_q['YearsCodePro']);
-
-print()
-print("Predicted values for one variable without outliers")
-print(reg_q.predict(df_reg_q[['YearsCode']]))
-
-y_pred1 = reg_q.predict(df_reg_q[['YearsCode']])
-y_true = df_reg_q['YearsCodePro']
-
-print()
-print("MSE for one variable without outliers:")
-print(mean_squared_error(y_true, y_pred1))
-
-#two variables without outliers
+# linear regression
 reg2_q = linear_model.LinearRegression()
-reg2_q.fit(df_reg_q[['YearsCode', 'Age']], df_reg_q['YearsCodePro']);
+reg2_q.fit(df_reg_q[['YearsCode', 'Age']], df_reg_q['YearsCodePro'])
 
 print()
 print("coef:")
@@ -216,12 +84,3 @@ y_true = df_reg_q['YearsCodePro']
 print()
 print("MSE for two variables without outliers:")
 print(mean_squared_error(y_true, y_pred))
-
-
-
-# reg3 = linear_model.LinearRegression()
-# reg3.fit(df_reg2[['YearsCode', 'Age', 'Hobbyist', 'Employment']], df_reg2['YearsCodePro']);
-
-# print(reg3.predict(df_reg2[['YearsCode', 'Age', 'Hobbyist', 'Employment']]))
-
-
